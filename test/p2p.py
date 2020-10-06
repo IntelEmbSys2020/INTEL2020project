ea06561e457046f38f853ec39b04fa8e24f1e652
studyHooligenVM <23731800@qq.com>
2020-10-06 16:34:11 +0800

    修改了p2p.py文件中if的bug

--- test/p2p.py
+++ test/p2p.py
@@ -60,7 +60,7 @@ class P2P:
         #等待接收作业端地址（TCP）
         print("等待服务器发送作业端IP...")
         recvData = self.socket_TCP.recv(16)
-        if(recvData.__len__ < 4)
+        if(len(recvData) < 4):
             print("接收作业端IP失败！")
             return False
         self.terminal_IPv4 = recvData.decode('utf-8')
